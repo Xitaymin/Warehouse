15748d90d0fbf060107a92effaa3e3ad7720dacf
Xitaymin <y.barashenko@gmail.com>
2021-04-11 22:54:35 +0300

    Made toString, equals and hashcode methods.

--- src/main/java/ua/training/model/Product.java
+++ src/main/java/ua/training/model/Product.java
@@ -27,10 +27,13 @@ public class Product implements Cloneable {
         this.manufacturerNumber = manufacturerNumber;
         this.price = price;
     }
-
+    /*
+    Constructor for instance when manufacturer is supplier
+     */
     public Product(String name, String manufacturer, int quantity, LocalDate manufacturingDate, Period shelfLife,
                    String manufacturerNumber, BigDecimal price) {
-        this(name,manufacturer,quantity,manufacturingDate,shelfLife,manufacturer,manufacturerNumber,manufacturerNumber,price);
+        this(name, manufacturer, quantity, manufacturingDate, shelfLife, manufacturer, manufacturerNumber,
+             manufacturerNumber, price);
     }
 
     public BigDecimal getPrice() {
@@ -106,7 +109,51 @@ public class Product implements Cloneable {
     }
 
     @Override
-    public Object clone() throws CloneNotSupportedException {
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Product{");
+        sb.append("name='").append(name).append('\'');
+        sb.append(", manufacturer='").append(manufacturer).append('\'');
+        sb.append(", quantity=").append(quantity);
+        sb.append(", manufacturingDate=").append(manufacturingDate);
+        sb.append(", shelfLife=").append(shelfLife);
+        sb.append(", supplier='").append(supplier).append('\'');
+        sb.append(", supplierNumber='").append(supplierNumber).append('\'');
+        sb.append(", manufacturerNumber='").append(manufacturerNumber).append('\'');
+        sb.append(", price=").append(price);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Product product = (Product) o;
+        return name.equals(product.name) && manufacturer.equals(product.manufacturer) &&
+               manufacturingDate.equals(product.manufacturingDate);
+    }
+
+    /*
+    For hashcode and equals were selected fields which values won't be probably changed during objects life.
+     */
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 31 * hash + (name == null ? 0 : name.hashCode());
+        hash = 31 * hash + (manufacturer == null ? 0 : manufacturer.hashCode());
+        hash = 31 * hash + (manufacturingDate == null ? 0 : manufacturingDate.hashCode());
+        return hash;
+    }
+
+    /*
+    All fields of this class are primitive or immutable, so there is no need in additional actions.
+     */
+    @Override
+    public Object clone() {
         try {
             return super.clone();
         } catch (CloneNotSupportedException e) {
